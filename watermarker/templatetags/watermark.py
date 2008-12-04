160d53707eee77dfe6022c6e2c64b6140cac2e6a
codekoala <none@none>
2008-12-04 16:55:48 +0000

    Refactored a lot of code from the watermark filter and placed it in the utils.py file.  Removed "parameter precedence" for positioning, tiling, and scaling.  Other useful improvements.
    --HG--
    extra : convert_revision : svn%3Ad2f3068a-bb81-11dd-ad1f-f1f9622dbc98/trunk%406

--- watermarker/templatetags/watermark.py
+++ watermarker/templatetags/watermark.py
@@ -16,35 +16,6 @@ try:
 except AttributeError:
     QUALITY = 85
 
-def _percent(var):
-    """
-    Just a simple interface to the _val function with a more meaningful name.
-    """
-    return _val(var, True)
-
-def _int(var):
-    """
-    Just a simple interface to the _val function with a more meaningful name.
-    """
-    return _val(var)
-
-def _val(var, is_percent=False):
-    """
-    Tries to determine the appropriate value of a particular variable that is
-    passed in.  If the value is supposed to be a percentage, a whole integer
-    will be sought after and then turned into a floating point number between
-    0 and 1.  If the value is supposed to be an integer, the variable is cast
-    into an integer.
-    """
-    try:
-        if is_percent:
-            var = float(int(var.strip('%')) / 100.0)
-        else:
-            var = int(var)
-    except ValueError:
-        raise template.TemplateSyntaxError('invalid watermark parameter: ' + var)
-    return var
-
 def _get_path_from_url(url, root=settings.MEDIA_ROOT, url_root=settings.MEDIA_URL):
     """
     Makes a filesystem path from the specified URL
@@ -88,17 +59,18 @@ def watermark(url, args=''):
             the watermark to "fully show".  This assumes the watermark is not
             as big as the original image.
         - just R for random placement
+        - just C to center the watermark
     - opacity::
         - X where X is an integer from 0 to 100.  This value represents the
             transparency level of the watermark when it is applied.
     - tile::
         - 0 or 1 to specify whether or not the watermark shall be tiled across
-            the entire image.  Takes precedence over the `position` parameter
-            but not the `scale` parameter.
+            the entire image.
     - scale::
-        - 0 or 1 to specify whether or not to scale the watermark to cover the
-            whole image.  Takes precedence over both the `position` and `tile`
-            parameters.
+        - a floating-point number above 0 to specify the scaling for the 
+            watermark.  If you want the watermark to be scaled to its maximum
+            without falling off the edge of the target image, use 'F'.  By 
+            default, scale is set to 1.0, or 1:1 scaling.
     - greyscale::
         - 0 or 1 to specify whether or not the watermark should be converted to
             a greyscale image before applying it to the target image.
@@ -106,10 +78,6 @@ def watermark(url, args=''):
         - 0 to 359 to specify the number of degrees to rotate the watermark
             before applying it to the target image.  Alternatively, you may
             set rotation=R for a random rotation value.
-
-    A full example for using this filter follows::
-
-        {{ image|watermark:"My Watermark,position=100x100,opacity=35,tile=0,scale=0,greyscale=1,rotation=45" }}
     """
 
     basedir = os.path.dirname(url) + '/'
@@ -119,14 +87,12 @@ def watermark(url, args=''):
     args = args.split(',')
     name = args.pop(0)
     top = left = 0
-    o_left = o_top = '0'
     opacity = 0.5
     tile = False
-    scale = False
+    scale = 1.0
     greyscale = False
     rotation = 0
-    pos = mode = None
-    b_edge = t_edge = l_edge = r_edge = False
+    position = None
 
     # look for the specified watermark by name.  If it's not there, go no further
     try:
@@ -138,109 +104,48 @@ def watermark(url, args=''):
     for arg in args:
         key, value = arg.split('=')
         if key == 'position':
-            # try to figure out what kind of positioning they want
-            pos = value.split('x')
-
-            if len(pos) != 1:
-                # relative and absolute positioning
-                o_left, o_top = pos
-
-                if '%' in o_left:
-                    left = _percent(o_left)
-                else:
-                    left = _int(o_left)
-
-                if '%' in o_top:
-                    top = _percent(o_top)
-                else:
-                    top = _int(o_top)
-            else:
-                if pos == 'R':
-                    # random positioning
-                    continue
-                else:
-                    # corner positioning--only take the first two characters
-                    pos = pos[0][:2]
-
-                    b_edge = 'b' in pos
-                    t_edge = 't' in pos
-                    l_edge = 'l' in pos
-                    r_edge = 'r' in pos
+            position = value
         elif key == 'opacity':
             opacity = _percent(value)
         elif key == 'tile':
             tile = bool(int(value))
         elif key == 'scale':
-            scale = bool(int(value))
+            scale = value
         elif key == 'greyscale':
             greyscale = bool(int(value))
         elif key == 'rotation':
-            if value.lower() == 'r':
-                rotation = random.randint(0, 359)
-            else:
-                rotation = _int(value)
+            rotation = value
 
-    # open the target image file
+    # open the target image file along with the watermark image
     target_path = _get_path_from_url(url)
     target = Image.open(target_path)
     mark = Image.open(watermark.image.path)
-
-    # determine the actual position based on the size of the image
-    max_left = target.size[0] - mark.size[0]
-    max_top = target.size[1] - mark.size[1]
-    if pos and pos == 'R':
-        # random positioning
-        left = random.randint(0, max_left)
-        top = random.randint(0, max_top)
-    elif b_edge or t_edge or l_edge or r_edge:
-        # corner positioning
-        if t_edge:
-            top = 0
-        elif b_edge:
-            top = max_top
-        if l_edge:
-            left = 0
-        elif r_edge:
-            left = max_left
-
-        if top < 0: top = 0
-        if left < 0: left = 0
-    else:
-        # relative/absolute positioning
-        if '%' in o_left:
-            left = int(max_left * left)
-
-        if '%' in o_top:
-            top = int(max_top * top)
-
-    mode = (left, top)
+    
+    # determine the actual value that the parameters provided will render
+    params = utils.determine_parameter_values(target, mark, position, opacity, scale, tile, greyscale, rotation)
+    params.update({
+                    'base': base,
+                    'watermark': watermark.id,
+                    'opacity': params['opacity'] * 100,
+                    'left': params['position'][0],
+                    'top': params['position'][1],
+                    'ext': ext
+                   })
 
     # come up with a good filename for this watermarked image
     wm_name = '%(base)s_wm_w%(watermark)i_o%(opacity)i_gs%(greyscale)i_r%(rotation)i'
-    if scale:
-        wm_name += '_scaled'
-        mode = 'scale'
-    elif tile:
+    if params['position']:
+        wm_name += '_p%(left)sx%(top)s'
+    if params['scale'] and params['scale'] != mark.size:
+        wm_name += '_s%i' % (float(params['scale'][0]) / mark.size[0] * 100)
+    if params['tile']:
         wm_name += '_tiled'
-        mode = 'tile'
-    else:
-        if b_edge or t_edge or l_edge or r_edge:
-            wm_name += '_p%s' % pos
-        else:
-            wm_name += '_p%(left)sx%(top)s'
     wm_name += '%(ext)s'
 
     # make thumbnail filename
-    wm_name = wm_name % {'base': base,
-                         'watermark': watermark.id,
-                         'opacity': opacity * 100,
-                         'rotation': rotation,
-                         'greyscale': greyscale,
-                         'left': left,
-                         'top': top,
-                         'ext': ext
-                        }
+    wm_name = wm_name % params
 
+    # figure out where the watermark would be saved on the filesystem
     new_file = urlparse.urljoin(basedir, wm_name)
     new_path = _get_path_from_url(new_file)
 
@@ -254,7 +159,14 @@ def watermark(url, args=''):
             return new_file
 
     # create the watermarked image on the filesystem
-    wm_image = utils.watermark(target, mark, mode, opacity, greyscale, rotation)
+    wm_image = utils.watermark(target, 
+                               mark, 
+                               position=position, 
+                               opacity=opacity, 
+                               scale=scale,
+                               tile=tile,
+                               greyscale=greyscale, 
+                               rotation=rotation)
     wm_image.save(new_path, quality=QUALITY)
 
     # send back the URL to the new, watermarked image
--- watermarker/utils.py
+++ watermarker/utils.py
@@ -1,56 +1,230 @@
 """
 Utilities for applying a watermark to an image using PIL.
 
-Source: http://code.activestate.com/recipes/362879/
+Original Source: http://code.activestate.com/recipes/362879/
 """
 import Image, ImageEnhance
+import random
 
-def reduce_opacity(im, opacity):
-    """Returns an image with reduced opacity."""
+def _percent(var):
+    """
+    Just a simple interface to the _val function with a more meaningful name.
+    """
+    return _val(var, True)
+
+def _int(var):
+    """
+    Just a simple interface to the _val function with a more meaningful name.
+    """
+    return _val(var)
+
+def _val(var, is_percent=False):
+    """
+    Tries to determine the appropriate value of a particular variable that is
+    passed in.  If the value is supposed to be a percentage, a whole integer
+    will be sought after and then turned into a floating point number between
+    0 and 1.  If the value is supposed to be an integer, the variable is cast
+    into an integer.
+    """
+    try:
+        if is_percent:
+            var = float(int(var.strip('%')) / 100.0)
+        else:
+            var = int(var)
+    except ValueError:
+        raise ValueError('invalid watermark parameter: ' + var)
+    return var
+
+def reduce_opacity(img, opacity):
+    """
+    Returns an image with reduced opacity.
+    """
     assert opacity >= 0 and opacity <= 1
 
-    if im.mode != 'RGBA':
-        im = im.convert('RGBA')
+    if img.mode != 'RGBA':
+        img = img.convert('RGBA')
     else:
-        im = im.copy()
+        img = img.copy()
 
-    alpha = im.split()[3]
+    alpha = img.split()[3]
     alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
-    im.putalpha(alpha)
+    img.putalpha(alpha)
 
-    return im
+    return img
 
-def watermark(img, mark, position, opacity=1, greyscale=False, rotation=0):
-    """Adds a watermark to an image."""
+def determine_scale(scale, img, mark):
+    """
+    Scales an image using a specified ratio or 'F'.  If `scale` is 'F', the 
+    image is scaled to be as big as possible to fit in `img` without falling off
+    the edges.  Returns the scaled `mark`.
+    """
+    
+    if scale:
+        try:
+            scale = float(scale)
+        except ValueError:
+            pass
+
+        if (isinstance(scale, str) or isinstance(scale, unicode)) \
+            and scale.lower() == 'f':
+            # scale, but preserve the aspect ratio
+            scale = min(
+                        float(img.size[0]) / mark.size[0], 
+                        float(img.size[1]) / mark.size[1]
+                       )
+        elif not isinstance(scale, float) and not isinstance(scale, int):
+            raise ValueError('Invalid scale value "%s"!  Valid values are 1) "F" for ratio-preserving scaling and 2) floating-point numbers and integers greater than 0.' % scale)
+
+        # determine the new width and height
+        w = int(mark.size[0] * scale)
+        h = int(mark.size[1] * scale)
+
+        # apply the new width and height, and return the new `mark`
+        return (w, h)
+    else:
+        return mark.size
+
+def determine_rotation(rotation, mark):
+    """
+    Determines the number of degrees to rotate the watermark image.
+    """
+    
+    if (isinstance(rotation, str) or isinstance(rotation, unicode)) \
+        and rotation.lower() == 'r':
+        rotation = random.randint(0, 359)
+    else:
+        rotation = _int(rotation)
+
+    return rotation
+
+def determine_position(position, img, mark):
+    """
+    Options:
+        TL: top-left
+        TR: top-right
+        BR: bottom-right
+        BL: bottom-left
+        C: centered
+        R: random
+        X%xY%: relative positioning on both the X and Y axes
+        X%xY: relative positioning on the X axis and absolute positioning on the 
+              Y axis
+        XxY%: absolute positioning on the X axis and relative positioning on the
+              Y axis
+        XxY: absolute positioning on both the X and Y axes
+    """
+    
+    max_left = img.size[0] - mark.size[0]
+    max_top = img.size[1] - mark.size[1]
+    
+    if not position:
+        position = 'r'
+
+    if isinstance(position, tuple):
+        left, top = position
+    elif isinstance(position, str) or isinstance(position, unicode):
+        position = position.lower()
+        
+        # corner positioning
+        if position in ['tl', 'tr', 'br', 'bl']:
+            if 't' in position:
+                top = 0
+            elif 'b' in position:
+                top = max_top
+            if 'l' in position:
+                left = 0
+            elif 'r' in position:
+                left = max_left
+
+        # center positioning
+        elif position == 'c':
+            left = int(max_left / 2)
+            top = int(max_top / 2)
+
+        # random positioning
+        elif position == 'r':
+            left = random.randint(0, max_left)
+            top = random.randint(0, max_top)
+
+        # relative or absolute positioning
+        elif 'x' in position:
+            left, top = position.split('x')
+            
+            if '%' in left:
+                left = max_left * _percent(left)
+            else:
+                left = _int(left)
+            
+            if '%' in top:
+                top = max_top * _percent(top)
+            else:
+                top = _int(top)
+
+    return (left, top)
+
+def determine_parameter_values(img, mark, position=(0, 0), opacity=1, scale=1.0, tile=False, greyscale=False, rotation=0):
+    """
+    Examines the input parameters to determine what the actual values will be 
+    for generating the watermark.
+    """
+    position = determine_position(position, img, mark)
+    scale = determine_scale(scale, img, mark)
+    rotation = determine_rotation(rotation, mark)
+    
+    return {
+        'position': position, 
+        'opacity':  opacity, 
+        'scale':    scale, 
+        'tile':     tile, 
+        'greyscale': greyscale, 
+        'rotation': rotation}
+
+def watermark(img, mark, position=(0, 0), opacity=1, scale=1.0, tile=False, greyscale=False, rotation=0, return_name=False):
+    """
+    Adds a watermark to an image.
+    """
     if opacity < 1:
         mark = reduce_opacity(mark, opacity)
+    
+    scale = determine_scale(scale, img, mark)
+    mark = mark.resize(scale)
+
+    if greyscale and mark.mode != 'LA':
+        mark = mark.convert('LA')
+    
+    rotation = determine_rotation(rotation, mark)
+    if rotation != 0:
+        # give soem leeway for rotation overlapping
+        new_w = mark.size[0] * 1.5
+        new_h = mark.size[1] * 1.5
+        
+        new_mark = Image.new('RGBA', (new_w, new_h), (0,0,0,0))
+        
+        # center the watermark in the newly resized image
+        new_l = (new_w - mark.size[0]) / 2
+        new_t = (new_h - mark.size[1]) / 2
+        new_mark.paste(mark, (new_l, new_t))
+        
+        mark = new_mark.rotate(rotation)
+    
+    position = determine_position(position, img, mark)
 
     if img.mode != 'RGBA':
         img = img.convert('RGBA')
 
-    if greyscale and mark.mode != 'LA':
-        mark = mark.convert('LA')
-
-    if rotation != 0:
-        rotmark = Image.new('RGBA', mark.size, (0,0,0,0))
-        rotmark.paste(mark, (0,0))
-        mark = rotmark.rotate(rotation)
+    # make sure we have a tuple for a position now
+    assert isinstance(position, tuple), 'Invalid position "%s"!' % position
 
     # create a transparent layer the size of the image and draw the
     # watermark in that layer.
     layer = Image.new('RGBA', img.size, (0,0,0,0))
-    if position == 'tile':
-        for y in range(0, img.size[1], mark.size[1]):
-            for x in range(0, img.size[0], mark.size[0]):
+    if tile:
+        first_y = position[1] % mark.size[1] - mark.size[1]
+        first_x = position[0] % mark.size[0] - mark.size[0]
+        
+        for y in range(first_y, img.size[1], mark.size[1]):
+            for x in range(first_x, img.size[0], mark.size[0]):
                 layer.paste(mark, (x, y))
-    elif position == 'scale':
-        # scale, but preserve the aspect ratio
-        ratio = min(
-            float(img.size[0]) / mark.size[0], float(img.size[1]) / mark.size[1])
-        w = int(mark.size[0] * ratio)
-        h = int(mark.size[1] * ratio)
-        mark = mark.resize((w, h))
-        layer.paste(mark, ((img.size[0] - w) / 2, (img.size[1] - h) / 2))
     else:
         layer.paste(mark, position)
 
@@ -60,9 +234,26 @@ def watermark(img, mark, position, opacity=1, greyscale=False, rotation=0):
 def test():
     im = Image.open('test.png')
     mark = Image.open('overlay.png')
-    watermark(im, mark, 'tile', 0.5, rotation=30).save('test1.png')
-    watermark(im, mark, 'scale', 1.0).save('test2.png')
-    watermark(im, mark, (100, 100), 0.5, True, -45).save('test3.png')
+    watermark(im, mark, 
+                tile=True, 
+                opacity=0.5, 
+                rotation=30).save('test1.png')
+
+    watermark(im, mark, 
+                scale='F').save('test2.png')
+
+    watermark(im, mark, 
+                position=(100, 100), 
+                opacity=0.5, 
+                greyscale=True, 
+                rotation=-45).save('test3.png')
+
+    watermark(im, mark, 
+                position='C',
+                tile=False, 
+                opacity=0.2,
+                scale=2, 
+                rotation=30).save('test4.png')
 
 if __name__ == '__main__':
     test()
